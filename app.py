0c7e67cd3414be84e5f58dc5f55bf78720951e25
jronald88 <156713858+jronald88@users.noreply.github.com>
2024-05-29 11:05:58 +0000

    Pending changes exported from your codespace

---
+++ app.py
@@ -0,0 +1,67 @@
+import pandas as pd
+
+#ex1
+#print("Hello World")
+
+data_frame = pd.read_csv(".learn/assets/pokemon_data.csv")
+"""
+print(data_frame)
+
+data = pd.Series([23,45,7,34,6,63,36,78,54,34])
+print(data)
+
+print(pd.date_range(start = "2021-05-01",end="2021-05-12"))
+
+my_series = pd.Series([2, 4, 6, 8, 10])
+print(my_series.apply(lambda x: x/2))
+"""
+#data = [["Toyota", "Corolla", "Blue"], ["Ford", "K", "Yellow"], ["Porsche", "Cayenne", "White"]]
+#df = pd.DataFrame(data, columns = ["Brand","Model","Color"])
+#print(df.head())
+
+data = [
+    { 
+        "brand": "Toyota", 
+        "model": "Corolla",
+        "color": "Blue"
+    },
+    {
+        "brand": "Ford", 
+        "model": "K",
+        "color": "Yellow"
+    },
+    {
+        "brand": "Porsche", 
+        "model": "Cayenne",
+        "color": "White"
+    },
+    
+]
+
+
+df = pd.DataFrame(data)
+df.loc[len(df.index)] = ["Tesla","Model S","Red"]
+#print(df)
+
+data_frame = pd.DataFrame(data_frame)
+#print(data_frame.iloc[133,6])
+"""
+print(data_frame.head(3))
+
+print(data_frame.tail(3))
+
+print(data_frame[['Name','Type 1']][0:10])
+
+print(data_frame.loc[data_frame['Attack']>80])
+
+print(len(data_frame.loc[data_frame['Legendary']]))
+"""
+
+US_baby_names = pd.read_csv('.learn/assets/us_baby_names_right.csv')
+print(US_baby_names.head())
+US_baby_names = US_baby_names.drop(columns = ['Unnamed: 0'])
+print(US_baby_names.head())
+
+print(US_baby_names.value_counts(US_baby_names['Gender']))
+
+print(len(US_baby_names.groupby(US_baby_names['Name']).sum()))
